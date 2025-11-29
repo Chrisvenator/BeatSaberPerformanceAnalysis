a87ee992f0756d6f1ce400500c29586286c31fcd
chrisvenator <55980003+Chrisvenator@users.noreply.github.com>
2025-11-29 16:28:27 +0100

    Added file to generalize loading the dataset

---
+++ dataset/load_dataset.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+def load_dataset(ranked=False):
+    data = pd.read_csv("./dataset/bs_scoring_dataset_76561198274713084.csv")
+    if ranked:
+        data = data[data["pp"] != 0]
+    print("Loaded Dataset with the shape: " + str(data.shape))
+    return data
