3ada51fdf3de3bd67df2c59f3f64d5ce967729b4
chrisvenator <55980003+Chrisvenator@users.noreply.github.com>
2025-12-02 16:35:47 +0100

    Added player performance over time grouped by Map tags

--- dataset/load_dataset.py
+++ dataset/load_dataset.py
@@ -2,14 +2,53 @@ from datetime import datetime
 
 import pandas as pd
 
+allowed_tags = [
+    "challenge",
+    "accuracy",
+    "speed",
+    "tech"
+    "poodle",
+    # "fitness",
+    "balanced",
+    # "dance",
+]
 
 def load_dataset(ranked=False):
     data = pd.read_csv("./dataset/bs_scoring_dataset_76561198274713084.csv")
+
     if ranked:
         data = data[data["pp"] > 0]
 
-        data["timeSet"] = pd.to_datetime(data["timeSet"], utc=True)
-        data = data[data["timeSet"] < pd.Timestamp("2024-05-01", tz="UTC")]
+    data = data[data["accuracy"] < 200]
 
-        print("Loaded Dataset with the shape: " + str(data.shape))
+    data["timeSet"] = pd.to_datetime(data["timeSet"], utc=True)
+    data = data[data["timeSet"] < pd.Timestamp("2024-03-01", tz="UTC")]
+    data = data[data["timeSet"] > pd.Timestamp("2020-12-01", tz="UTC")]
+
+    ct = 0
+    map_tags = []
+
+    for tags_list in data["tags"]:
+        map_tag = None
+
+        if pd.notna(tags_list):
+            tags_list = str(tags_list)
+            tags_list = tags_list.replace("[", "").replace("]", "").replace("'", "")
+            tags = [t.strip() for t in tags_list.split(",")]
+
+            for tag in tags:
+                for allowed_tag in allowed_tags:
+                    if tag == allowed_tag:
+                        ct += 1
+                        map_tag = tag
+                        break
+                if map_tag is not None:
+                    break
+
+        map_tags.append(map_tag)
+
+    data["map_tag"] = map_tags
+    print("Found " + str(ct) + " Maps with Tags")
+
+    print("Loaded Dataset with the shape: " + str(data.shape))
     return data
